435613cea7ba3ff08c4d4d53369a0db1abcf40e1
Zwe Linux <zwelinux@Zwes-Air.lan>
2022-12-26 10:26:50 +0630

    26_12_2022_commit_5

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     title: `zwelinux`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
