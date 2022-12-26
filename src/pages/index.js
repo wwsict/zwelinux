5cd0ac3b174ab4f446c5a7f38ead6087e374e9ac
Zwe Linux <zwelinux@Zwes-Air.lan>
2022-12-26 10:36:42 +0630

    26_12_2022_commit_6

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,11 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
-      <StaticImage
+      {/* <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
         src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
-      />
+      /> */}
+      <iframe width="100%" height="370" src="https://www.youtube.com/embed/j5V6vq07gPY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </Layout>
   )
 }
