737ff52bc8cf94f9f145bb33b56aeaa6dcc17bc1
Zwe Linux <zwelinux@Zwes-Air.lan>
2022-12-26 15:09:41 +0630

    26_12_2022_commit_8

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import * as React from 'react'
 import Layout from '../components/layout'
-import { StaticImage } from 'gatsby-plugin-image'
 import Seo from '../components/seo'
-
 
 const IndexPage = () => {
   return (
