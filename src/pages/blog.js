5eb8b1f4b4a18a1b46432f9ec034722aa33f3ead
Zwe Linux <zwelinux@Zwes-Air.lan>
2022-12-26 15:32:53 +0630

    26_12_2022_commit_9

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,33 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
+import { graphql } from 'gatsby'
 
-const BlogPage = () => {
+const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <p>My cool posts will go in here</p>
+      <ul>
+      {
+        data.allFile.nodes.map(node => (
+          <li key={node.name}>
+            {node.name}
+          </li>
+        ))
+      }
+      </ul>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`
 
 export const Head = () => <Seo title="My Blog Posts" />
 
